c2791a1ac09957abb92292d41ae671c7ac2d327e
Carlos Alberto Lemes <39310873+Carlosllemes@users.noreply.github.com>
2024-08-06 22:20:52 -0300

    fix readme faucet

--- src/creatWallet.js
+++ src/creatWallet.js
@@ -1,13 +1,12 @@
-// Importando as dependências necessárias
-const bip32 = require('bip32');  // Biblioteca para criar e gerenciar carteiras HD (Hierarchical Deterministic)
-const bip39 = require('bip39');  // Biblioteca para gerar mnemonics (frases mnemônicas) para seeds
-const bitcoin = require('bitcoinjs-lib');  // Biblioteca principal para interagir com a rede Bitcoin
+const bip32 = require('bip32');
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
 
 // Definindo a rede Bitcoin de teste (testnet)
 const network = bitcoin.networks.testnet;
 
-// Caminho de derivação para carteiras HD (conforme o padrão BIP49)
-const path = `m/49'/1'/0'/0`;
+// Caminho de derivação para carteiras HD SegWit nativo (BIP84) para poder usar o faucet https://bitcoinfaucet.uo1.net/send.php
+const path = `m/84'/1'/0'/0`;
 
 // Gerando uma frase mnemônica (12 palavras) para a seed
 let mnemonic = bip39.generateMnemonic();
@@ -21,17 +20,17 @@ let root = bip32.fromSeed(seed, network);
 // Derivando um caminho específico na carteira HD para criar uma conta
 let account = root.derivePath(path);
 
-// Derivando a primeira conta (conta 0) e a primeira chave dessa conta
+// Derivando a primeira chave pública dessa conta
 let node = account.derive(0).derive(0);
 
-// Criando um endereço de Bitcoin a partir da chave pública da conta derivada
-let btcAddress = bitcoin.payments.p2pkh({
+// Criando um endereço de Bitcoin SegWit nativo a partir da chave pública
+let btcAddress = bitcoin.payments.p2wpkh({
     pubkey: node.publicKey,
     network: network,
 }).address;
 
 // Exibindo informações sobre a carteira gerada
 console.log("Carteira gerada");
-console.log("Endereço: ", btcAddress);  // Endereço Bitcoin gerado
-console.log("Chave privada: ", node.toWIF());  // Chave privada da conta em formato WIF (Wallet Import Format)
-console.log("Seed: ", mnemonic);  // Frase mnemônica usada para gerar a seed
+console.log("Endereço: ", btcAddress);
+console.log("Chave privada: ", node.toWIF());
+console.log("Seed: ", mnemonic);
